bec74a3716123c3c61d2a8c1130fe3a4c1e66a22
bcube <bcube.hk@gmail.com>
2019-04-01 14:22:29 +0800

    add nuodb.js

---
+++ nuodb.js
@@ -0,0 +1,21 @@
+var nuodb = require('db-nuodb');
+
+console.log ("Starting....");
+
+new nuodb.Database({
+    hostname: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'node'
+}).connect(function(error) {
+    if (error) {
+        return console.log("CONNECTION ERROR: " + error);
+    }
+ 
+    this.query().select('*').from('users').execute(function(error, rows) {
+        if (error) {
+            return console.log('ERROR: ' + error);
+        }
+        console.log(rows.length + ' ROWS');
+    });
+});
